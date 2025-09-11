72297093e356c3883fa6686d431d76befd47fe2c
Tiago Rocha <tiago.joel.rocha023@gmail.com>
2025-09-11 19:12:39 -0300

    Inicio. Traer este commit para empezar

--- backend/usuarios/models.py
+++ backend/usuarios/models.py
@@ -2,9 +2,9 @@ from django.db import models
 import secrets
 
 class Usuario(models.Model):
-    nombre = models.CharField(max_length=100),
-    apellido = models.CharField(max_length=100),
-    email = models.EmailField(unique=True),
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
 
 
 # Guardamos la contraseña hasheada acá (nunca en texto plano)
