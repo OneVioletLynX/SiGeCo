2a1abc7830ea1cb2bc2c8fe5b90a92b0237fe04c
Tiago Rocha <tiago.joel.rocha023@gmail.com>
2025-09-11 21:35:00 -0300

    Creación Endpoint alumnos

---
+++ backend/alumnos/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers # Importa los serializers de DRF
+from .models import Alumno # Importa tu modelo 
+
+class AlumnoSerializer(serializers.ModelSerializer): # Creamos un serializer para el modelo 
+
+    class Meta:
+        model = Alumno
+        fields = [
+            'id', 'nombre', 'apellido', 'dni', 'email', 'ciudad', 'direccion', 'telefono',
+            'telefono_respaldo', 'inscripcion', 'fecha_nacimiento'
+        ]
+        read_only_fields = ['id']
