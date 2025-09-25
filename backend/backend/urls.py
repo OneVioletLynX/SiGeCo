8b5621284c4b5f470d9b7dec2c912db852739118
Tiago Rocha <tiago.joel.rocha023@gmail.com>
2025-09-24 21:33:07 -0300

    login de backend

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin 
 from django.urls import path 
-from usuarios.views import UsuarioListCreate, UsuarioDetail
+from usuarios.views import UsuarioListCreate, UsuarioDetail, LoginView
 from alumnos.views import AlumnoListCreate, AlumnoDetail
 from valores.views import ValoresListCreate, ValoresDetail, ConceptoListCreate, ConceptoDetail
 
@@ -10,6 +10,7 @@ urlpatterns = [
     path('api/usuarios/', UsuarioListCreate.as_view(), name='usuarios-lista'), 
     # Para operaciones sobre un usuario específico 
     path('api/usuarios/<int:pk>/', UsuarioDetail.as_view(), name='usuario-detalle'), 
+    path('api/login/', LoginView.as_view(), name='login'),
 
     path('api/alumnos/', AlumnoListCreate.as_view(), name='alumnos-lista'), 
     # Para operaciones sobre un alumnos específico 
